43b363541556e1990b8e135273d8b59360924be7
DevCorvus <devcorvus.contact@gmail.com>
2023-08-12 02:16:57 -0400

    feat: command to list pacman packages sorted by size

--- .oh-my-zsh/custom/devcorvus.zsh
+++ .oh-my-zsh/custom/devcorvus.zsh
@@ -7,6 +7,7 @@ alias wd='cd ~/Dev/code/projects'
 alias edit:zsh='nvim ~/.oh-my-zsh/custom/devcorvus.zsh'
 alias edit:nvim='nvim ~/.config/nvim'
 alias edit:hosts='sudo nvim /etc/hosts'
+alias pacman:list='pacman -Qi | egrep "^(Name|Installed)" | cut -f2 -d":" | paste - - | column -t | sort -nrk 2 | grep MiB | less'
 alias pacman:clear='sudo pacman -Rns $(pacman -Qtdq)'
 alias pacman:cache='paccache -r --keep 2'
 # tmux
